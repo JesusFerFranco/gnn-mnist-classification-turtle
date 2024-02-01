5389f67cd6b7243478e3b46243261385da0836a0
JesusFerFranco <jffl49560@gmail.com>
2024-01-31 18:21:49 -0600

    datasets.py

--- gnn_image_classification/datasets.py
+++ gnn_image_classification/datasets.py
@@ -28,7 +28,7 @@ class TURTLESuperpixels(InMemoryDataset):
     def __init__(
         self, root, transform= None, pre_transform = None,
     ) -> None:
-        super(TURTLESuperpixels).__init__(root, transform, pre_transform)
+        super(TURTLESuperpixels).__init__(self, root, transform, pre_transform)
 
     @property
     def raw_file_names(self) -> str:
