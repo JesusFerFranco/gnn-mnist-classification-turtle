6003f68e44b8da6c04f6df879534467532e6e60f
JesusFerFranco <jffl49560@gmail.com>
2024-02-03 19:08:26 -0600

    datasets.py

--- gnn_image_classification/datasets.py
+++ gnn_image_classification/datasets.py
@@ -124,8 +124,8 @@ def build_train_val_dataloaders(batch_size: int, device: str) -> tuple[DataLoade
     
 #    train_dataset =  torch.load(train_filename)
  #   val_dataset = torch.load(test_filename)
-    train_dataset=build_mnist_superpixels_dataset()
-    val_dataset=build_mnist_superpixels_dataset()
+   # train_dataset=build_mnist_superpixels_dataset()
+    #val_dataset=build_mnist_superpixels_dataset()
     train_loader = build_dataloader(
         dataset=train_dataset,
         batch_size=batch_size,
